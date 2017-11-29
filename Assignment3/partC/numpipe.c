d1ecd2ecf5f22791323b84c8052bfe98949cc4fc
xianng <valleyzhx@gmail.com>
2017-11-29 15:10:59 -0500

    GFP_KERNEL

--- Assignment3/partC/numpipe.c
+++ Assignment3/partC/numpipe.c
@@ -126,7 +126,7 @@ int __init my_init(void)
     sema_init(&_mutex, 1);
     sema_init(&_full, 0);
     sema_init(&_empty, N);
-    _buffer = kmalloc(N*sizeof(int),GFP_ATOMIC);
+    _buffer = kmalloc(N*sizeof(int),GFP_KERNEL);
     if (error) {
         printk(KERN_ALERT "misc_register error: %d!\n",error);
         return error;
