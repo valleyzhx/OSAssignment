f8465fe93122e39dc5b661ce6542511b2d5ed425
xianng <valleyzhx@gmail.com>
2017-11-29 17:07:29 -0500

    remove _index_lock

--- Assignment3/partC/numpipe.c
+++ Assignment3/partC/numpipe.c
@@ -22,7 +22,7 @@ static struct semaphore _mutex;//lock
 static struct semaphore _empty;
 static struct semaphore _full;
 
-static struct semaphore _index_lock;
+//static struct semaphore _index_lock;
 
 static int _open_count = 0;
 static int *_buffer;
@@ -70,13 +70,13 @@ static ssize_t my_read(struct file *file, int __user *out, size_t len, loff_t *p
         int process = _buffer[0];
         err = copy_to_user(out,&process,len);
         if (err == SUCCESS) {
-            down_interruptible(&_index_lock);
+            //down_interruptible(&_index_lock);
             int i=0;
             for (i=0; i<_count-1; i++) {
                 _buffer[i] = _buffer[i+1];
             }
             _count--;
-            up(&_index_lock);
+            //up(&_index_lock);
 
             printk(KERN_ALERT "read from %d, now items number: %d\n", process,_count);
         }else{
@@ -100,10 +100,10 @@ static ssize_t my_write(struct file *file, int __user *buf,
     int process;
     int err = copy_from_user(&process,buf,len);
     if (err == SUCCESS) {
-        down_interruptible(&_index_lock);
+        //down_interruptible(&_index_lock);
         _buffer[_count] = process;
         _count++;
-        up(&_index_lock);
+        //up(&_index_lock);
 
         printk(KERN_ALERT "write process %d, now items number: %d\n", process,_count);
     }else{
