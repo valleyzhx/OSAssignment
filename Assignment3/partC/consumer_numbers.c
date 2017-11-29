13a7e2a9127ed3d32198d650566bdf1791924b19
xianng <valleyzhx@gmail.com>
2017-11-29 10:58:27 -0500

    consumer sleep

--- Assignment3/partC/consumer_numbers.c
+++ Assignment3/partC/consumer_numbers.c
@@ -30,8 +30,8 @@ int main(int argc, char *argv[])
 		} else {
 			fprintf(stderr, "error reading ret=%ld errno=%d perror: ", ret, errno);
 			perror("");
-			sleep(1);
 		}
+        sleep(1);
 	}
 	close(fd);
 
